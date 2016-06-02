27c5e307f86dcf63205537a11d1c2f919e8349b7
simone preite <simone-preite_89@hotmail.it>
2016-06-02 23:36:01 +0200

    first commit

---
+++ bot.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+import time
+import telepot
+import subprocess
+from time import sleep
+import commands
+
+def handle(msg):
+	dict_msg = msg
+	commands.__command__(dict_msg["from"]["id"], dict_msg["text"],bot)
+
+dict_msg = dict()
+string_msg = dict()
+message = ""
+user = ""
+
+TOKEN = '233835233:AAEBsmLtNmXdbc5G811eoGuYzOIpkS1LBgk'
+
+bot = telepot.Bot(TOKEN)
+bot.message_loop(handle)
+print ('Listening ...')
+# Keep the program running.
+while 1:
+    time.sleep(10)
---
+++ commands.py
@@ -0,0 +1,20 @@
+import random
+import os
+from time import sleep
+
+def __command__(id,message,bot):
+	dict_command = {'/up_server':['up_fisso','server on line a breve'],  '/reboot_board':['shutdown -r 1','rebooting....'], '/poweroff_board':['shutdown -h `"now + 10 seconds"`','poweroff....']}
+	list_insulti = ['sei un cane','attaccati al cazzo','testa di minchia','gaydimmerda','fatti sfondare da un nero','porcamadonna non puoi']
+	time = 5
+	if (id == 71120657):
+		if message in dict_command.keys():
+			os.system(dict_command[message][0])
+			sleep(time/100)
+			bot.sendMessage(id, dict_command[message][1])
+		else:
+			sleep(time/100)
+			bot.sendMessage(id, 'comando non previsto!')
+	else:
+		n = random.randint(0, 5)
+		sleep(time/100)
+		bot.sendMessage(id, list_insulti[n])
