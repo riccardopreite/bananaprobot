a69d600f6ebbbd0e15d4420a688218e6e0cd05fe
simone preite <simone-preite_89@hotmail.it>
2016-06-13 21:20:26 +0200

    up

--- bot.py
+++ bot.py
@@ -6,8 +6,7 @@ import subprocess
 from time import sleep
 import commands
 
-def handle(msg):
-	ID = sys.argv[2]
+def handle(msg,ID):
 	dict_msg = msg
 	commands.__command__(dict_msg["from"]["id"], dict_msg["text"], bot, ID)
 
@@ -18,9 +17,10 @@ message = ""
 
 #TOKEN = '233835233:AAEBsmLtNmXdbc5G811eoGuYzOIpkS1LBgk'
 TOKEN = sys.argv[1]
+ID = sys.argv[2]
 
 bot = telepot.Bot(TOKEN)
-bot.message_loop(handle)
+bot.message_loop(handle, ID)
 print ('Listening ...')
 # Keep the program running.
 while 1:
